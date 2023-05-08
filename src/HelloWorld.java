ab5e6f983b70b5f662f77201ba6435733e0a34cb
Aemen Mohsin <aemen.mohsin@ucalgary.ca>
2023-05-08 06:10:58 -0400

    changed greeting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		//TODO: Change to greet you instead of the world.
-		System.out.println("Hello World!");
+		System.out.println("Hello Aemen!");
 		
 		System.out.println("Java version is : " + System.getProperty("java.version"));
 
